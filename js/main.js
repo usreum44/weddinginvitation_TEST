5c42e20cc1bdd7c3d6264422baa71e512b63d52b
usreum44 <us.reum@gmail.com>
2026-07-30 00:12:15 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -628,34 +628,6 @@ window.openContact = openContact;
 window.closeContact = closeContact;
 
 
-
-// ===================================
-// 9. POPUP TEST
-// ===================================
-     
-const modal = document.querySelector('.modal');
-const modalOpen = document.querySelector('.modal_btn');
-const modalClose = document.querySelector('.close_btn');
-
-// //열기 버튼을 눌렀을 때 모달팝업이 열림
-// modalOpen.addEventListener('click',function(){
-//     modal.classList.add('on');
-// });
-// //닫기 버튼을 눌렀을 때 모달팝업이 닫힘
-// modalClose.addEventListener('click',function(){
-//     modal.classList.remove('on');
-// });
-
-// 2. 스타일 직접 변경
-//열기 버튼을 눌렀을 때 모달팝업이 열림
-modalOpen.addEventListener('click',function(){
-    modal.style.display = 'block';
-});
-//닫기 버튼을 눌렀을 때 모달팝업이 닫힘
-modalClose.addEventListener('click',function(){
-    modal.style.display = 'none';
-});
-
      
     }
 );
